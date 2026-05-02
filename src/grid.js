964ae2f55a65cdee54a27c52fac9cd4cf1efb283
Valentin Marcel <valentinmarcel39@gmail.com>
2026-05-02 23:22:05 +0200

    Update selectionable rules: max opponent boxing advantage 2

--- src/grid.js
+++ src/grid.js
@@ -104,6 +104,7 @@ export function draw(canvas, ctx, myChess, myBox, showProbableFighters, strictMa
     for (let j = 0; j < NY; j++) {
       for (let i = 0; i < NX; i++) {
         const boxDiff = Math.abs(myBox - boxLevels[j]);
+        const oppBoxAdvantage = boxLevels[j] - myBox;
         const { expectedRounds, chessWin, boxWin } = getWinBreakdown(myChess - chessLevels[i], myBox - boxLevels[j]);
         const winProb = chessWin + boxWin;
         
@@ -111,7 +112,7 @@ export function draw(canvas, ctx, myChess, myBox, showProbableFighters, strictMa
         if (strictMatchmaking) {
           isInvalid = boxDiff > 1 || winProb > 0.70 || winProb < 0.30 || expectedRounds < 4.0;
         } else if (showProbableFighters) {
-          isInvalid = boxDiff > 2 || winProb > 0.90;
+          isInvalid = oppBoxAdvantage > 2 || winProb > 0.90;
         }
 
         if (isInvalid) {
